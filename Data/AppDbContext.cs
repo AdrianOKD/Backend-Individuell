ed27e2033db31f5d59f5ee0890260674aa1e8f1e
AdrianOKD <adrian.karl.oskar.dahl@gmail.com>
2025-05-16 14:31:24 +0200

    Set up dbcontext relationships

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options) { }
+
+    public DbSet<FolderEntity> Folder { get; set; }
+
+    public DbSet<FileEntity> Files { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder
+            .Entity<FolderEntity>()
+            .HasOne(f => f.Owner)
+            .WithMany(u => u.OwnedFolders)
+            .HasForeignKey(f => f.OwnerId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder
+            .Entity<FileEntity>()
+            .HasOne(f => f.Owner)
+            .WithMany(u => u.OwnedFiles)
+            .HasForeignKey(f => f.OwnerId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder
+            .Entity<FolderEntity>()
+            .HasOne(f => f.ParentFolder)
+            .WithMany(f => f.SubFolder)
+            .HasForeignKey(f => f.ParentFolderId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder
+            .Entity<FileEntity>()
+            .HasOne(f => f.Folder)
+            .WithMany(f => f.Files)
+            .HasForeignKey(f => f.FolderId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
