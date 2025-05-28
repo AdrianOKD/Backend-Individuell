51f447f91695aa901792a9419f37247c889ec87b
AdrianOKD <adrian.karl.oskar.dahl@gmail.com>
2025-05-28 20:12:40 +0200

    Feat: Add scope to IFolderService/FolderService

--- Program.cs
+++ Program.cs
@@ -2,13 +2,13 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
-
 public class Program
 {
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
 
+        builder.Services.AddScoped<IFolderService, FolderService>();
         builder.Services.AddControllers();
         builder.Services.AddAuthorization();
         builder.Services.AddOpenApi();
