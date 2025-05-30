0119d9c3996d09e1812c77fe017ae522e7f12eaf
AdrianOKD <adrian.karl.oskar.dahl@gmail.com>
2025-05-30 11:09:27 +0200

    Refactor: add null check and comment for navigation prop

--- Models/Entities/FileEntity.cs
+++ Models/Entities/FileEntity.cs
@@ -3,11 +3,13 @@ public class FileEntity
     public int Id { get; set; }
     public string Name { get; set; }
     public byte[] Content { get; set; }
+
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime ModifiedAt { get; set; } = DateTime.UtcNow;
-
     public string OwnerId { get; set; }
-    public UserEntity Owner { get; set; }
     public int FolderId { get; set; }
+
+    //Navigation Prop
+    public UserEntity Owner { get; set; }
     public FolderEntity Folder { get; set; }
 }
--- Models/Entities/UserEntity.cs
+++ Models/Entities/UserEntity.cs
@@ -2,9 +2,10 @@ using Microsoft.AspNetCore.Identity;
 
 public class UserEntity : IdentityUser
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
 
+    //Navigation Prop
     public ICollection<FolderEntity> OwnedFolders { get; set; } = new List<FolderEntity>();
     public ICollection<FileEntity> OwnedFiles { get; set; } = new List<FileEntity>();
 }
