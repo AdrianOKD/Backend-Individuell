e769a1d65c8aba87eb0680ba6707bee8ba852e20
AdrianOKD <adrian.karl.oskar.dahl@gmail.com>
2025-06-04 09:36:30 +0200

    Feat: Initialize methods for IFileService. -add interface methods

--- Services/FilesService.cs
+++ Services/FilesService.cs
@@ -1,8 +1,27 @@
+
 public class FileService : IFileService
 {
     private readonly IFileRepository fileRepository;
     private readonly IFolderRepository folderRepository;
     private readonly ILogger<FileService> logger;
 
-    public FileService(IFileRepository fileRepository, IFolderRepository folderRepository) { }
+    public FileService(IFileRepository fileRepository, IFolderRepository folderRepository)
+    { 
+        
+    }
+
+    public Task<FileEntity> GetFileAsync(int id, string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<FileEntity> RegisterFileAsync(CreateFileRequest request, string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RemoveFileAsync(int id, string userId)
+    {
+        throw new NotImplementedException();
+    }
 }
--- Services/Interfaces/IFileService.cs
+++ Services/Interfaces/IFileService.cs
@@ -1,4 +1,6 @@
-public interface IFileService 
+public interface IFileService
 {
-    
+    Task<FileEntity> RegisterFileAsync(CreateFileRequest request, string userId);
+    Task<FileEntity> GetFileAsync(int id, string userId);
+    Task RemoveFileAsync(int id, string userId);
 }
