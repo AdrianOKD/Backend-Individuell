2004d8b13579c76e9221beddf0ff5af361ebeb24
AdrianOKD <adrian.karl.oskar.dahl@gmail.com>
2025-06-04 10:25:48 +0200

    Refactor: Removed unnecessary variable

--- Models/Entities/FolderEntity.cs
+++ Models/Entities/FolderEntity.cs
@@ -3,12 +3,10 @@ public class FolderEntity
     public int Id { get; set; }
     public required string Name { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    public DateTime ModifiedAt { get; set; } = DateTime.UtcNow;
+
     public required string OwnerId { get; set; }
 
-    //Navigation
+    //Navigation prop
     public UserEntity? Owner { get; set; }
     public ICollection<FileEntity> Files { get; set; } = new List<FileEntity>();
 }
-
-
