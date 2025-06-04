adbddbc60b81b9df3d53aa7159497c161677fb6f
AdrianOKD <adrian.karl.oskar.dahl@gmail.com>
2025-06-04 10:27:12 +0200

    Fix: FolderDto -FolderDto no longer include unecessary variables not inteded for response. -Improved mapping

--- Models/DTOs/Responses/FolderDto.cs
+++ Models/DTOs/Responses/FolderDto.cs
@@ -2,18 +2,9 @@ public class FolderDto
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public int? ParentFolderId { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public DateTime ModifiedAt { get; set; }
 
     public static FolderDto Map(FolderEntity response)
     {
-        return new FolderDto
-        {
-            Id = response.Id,
-            Name = response.Name,
-            CreatedAt = DateTime.UtcNow,
-            ModifiedAt = DateTime.UtcNow,
-        };
+        return new FolderDto { Id = response.Id, Name = response.Name };
     }
 }
