2e6373ca11f44d973bff2168244d0da9097d28d0
AdrianOKD <adrian.karl.oskar.dahl@gmail.com>
2025-06-05 15:56:35 +0200

    Feat: Add mapping to FileDto

--- Models/DTOs/Responses/FileDto.cs
+++ Models/DTOs/Responses/FileDto.cs
@@ -3,6 +3,18 @@ public class FileDto
     public int Id { get; set; }
     public string Name { get; set; }
     public long Size { get; set; }
+    public DateTime CreatedAt { get; set; }
+
     public int FolderId { get; set; }
     public string FolderName { get; set; }
+
+    public static FileDto Map(FileEntity response)
+    {
+        return new FileDto
+        {
+            Id = response.Id,
+            Name = response.Name,
+            CreatedAt = response.CreatedAt,
+        };
+    }
 }
