03d098fbc73c750721c2dada85dd0b9164aaae05
AdrianOKD <adrian.karl.oskar.dahl@gmail.com>
2025-06-05 15:57:40 +0200

    Feat: Add basic method for mapping file

--- Services/FilesService.cs
+++ Services/FilesService.cs
@@ -1,21 +1,27 @@
-
 public class FileService : IFileService
 {
     private readonly IFileRepository fileRepository;
     private readonly IFolderRepository folderRepository;
-    private readonly ILogger<FileService> logger;
+    private readonly ILogger<IFileService> logger;
 
-    public FileService(IFileRepository fileRepository, IFolderRepository folderRepository)
-    { 
-        
+    public FileService(
+        IFileRepository fileRepository,
+        IFolderRepository folderRepository,
+        Logger<IFileService> logger
+    )
+    {
+        this.fileRepository = fileRepository;
+        this.folderRepository = folderRepository;
+        this.logger = logger;
+    }
+
+    public async Task<FileEntity> RegisterFileAsync(CreateFileRequest request, string userId)
+    {
+        var file = CreateFileRequest.ToEntityMap(request, userId);
+        return await fileRepository.CreateFileAsync(file);
     }
 
     public Task<FileEntity> GetFileAsync(int id, string userId)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<FileEntity> RegisterFileAsync(CreateFileRequest request, string userId)
     {
         throw new NotImplementedException();
     }
