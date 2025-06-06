367da396f79125cb6155f0809751be5150e4ddab
AdrianOKD <adrian.karl.oskar.dahl@gmail.com>
2025-06-06 11:37:47 +0200

    Fix: Null checks

--- Models/Entities/FileEntity.cs
+++ Models/Entities/FileEntity.cs
@@ -1,14 +1,14 @@
 public class FileEntity
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public byte[] Content { get; set; }
+    public string Name { get; set; } = null!;
+    public byte[] Content { get; set; } = null!;
 
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    public string OwnerId { get; set; }
+    public string OwnerId { get; set; } = null!;
     public int FolderId { get; set; }
 
     //Navigation Prop
-    public UserEntity Owner { get; set; }
-    public FolderEntity Folder { get; set; }
+    public UserEntity Owner { get; set; } = null!;
+    public FolderEntity Folder { get; set; } = null!;
 }
