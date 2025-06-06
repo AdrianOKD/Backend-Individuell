c9468a9083bdcc1e89be3f473767605ffbc9bccf
AdrianOKD <adrian.karl.oskar.dahl@gmail.com>
2025-06-06 18:44:19 +0200

    Feat: Custom exceptions for file and folder

--- Exceptions/FileExceptions.cs
+++ Exceptions/FileExceptions.cs
@@ -1,1 +1,5 @@
-public class FileExceptions{}
+public class FileNotFoundException : Exception
+{
+    public FileNotFoundException(int id)
+        : base($"Folder with ID: '{id}' was not found.") { }
+}
