975a30cccc9f3c113ee4c8882bbae14be8b7aaa2
AdrianOKD <adrian.karl.oskar.dahl@gmail.com>
2025-06-07 13:22:23 +0200

    Fix:  Set required

--- Models/Entities/FileEntity.cs
+++ Models/Entities/FileEntity.cs
@@ -1,12 +1,12 @@
 public class FileEntity
 {
     public int Id { get; set; }
-    public string Name { get; set; } = null!;
-    public byte[] Content { get; set; } = null!;
+    public required string Name { get; set; }
+    public required byte[] Content { get; set; }
 
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    public string OwnerId { get; set; } = null!;
-    public int FolderId { get; set; }
+    public required string OwnerId { get; set; }
+    public required int FolderId { get; set; }
 
     //Navigation Prop
     public UserEntity Owner { get; set; } = null!;
