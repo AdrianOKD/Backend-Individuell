cca72bba7a4981f6a2c6710f6c0ddf2dc0f1b078
AdrianOKD <adrian.karl.oskar.dahl@gmail.com>
2025-06-08 16:16:38 +0200

    Refactor: Removed public in interface because of redundancy

--- Data/Repositories/Interfaces/IFileRepository.cs
+++ Data/Repositories/Interfaces/IFileRepository.cs
@@ -1,19 +1,15 @@
 public interface IFileRepository
 {
-    public Task<FileEntity> CreateFileAsync(FileEntity file);
+    Task<FileEntity> CreateFileAsync(FileEntity file);
 
-    public Task<FileEntity> FetchFileAsync(int id, string userId);
+    Task<FileEntity> FetchFileAsync(int id, string userId);
 
-    public Task<(List<FolderEntity> folders, List<FileEntity> files)> FetchAllFoldersWithFilesAsync(
+    Task<(List<FolderEntity> folders, List<FileEntity> files)> FetchAllFoldersWithFilesAsync(
         string userId
     );
-    public Task<FileEntity> FetchFileForDuplicateCheckAsync(
-        string fileName,
-        int folderId,
-        string userId
-    );
+    Task<FileEntity> FetchFileForDuplicateCheckAsync(string fileName, int folderId, string userId);
 
-    public Task<FileEntity> UpdateFileAsync(FileEntity file);
+    Task<FileEntity> UpdateFileAsync(FileEntity file);
 
-    public Task DeleteFileAsync(FileEntity file);
+    Task DeleteFileAsync(FileEntity file);
 }
